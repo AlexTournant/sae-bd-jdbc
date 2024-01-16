94a2e6fe86306dbfe9154b8f55e5c15aad956cc3
DEMORY Mael <mael_demory@ens.univ-artois.fr>
2024-01-16 18:01:40 +0100

    Ajout des méthodes permettant d'ajouter et gérer les membres participant à chaque projet et permettant de passer un membre responsable d'un projet

--- src/BD/Requetes.java
+++ src/BD/Requetes.java
@@ -4,7 +4,112 @@ import java.sql.*;
 
 public class Requetes {
 
-	
+    public static void deleteMember(String nomDB, int idMembre, int idProjet) throws SQLException {
+        Connection connexion = null;
+        try {
+            connexion = CreationBD.connexionBD(nomDB);
+            String sql = "DELETE FROM ProjetMembre WHERE id_projet = ? AND id_membre = ?";
+
+            try (PreparedStatement statement = connexion.prepareStatement(sql)) {
+                statement.setInt(1, idProjet);
+                statement.setInt(2, idMembre);
+
+                int lines = statement.executeUpdate();
+
+                if (lines > 0) {
+                    System.out.println("Attribution du membre au projet supprimée avec succès.");
+                } else {
+                    System.out.println("Aucune attribution correspondante trouvée.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            CreationBD.fermerConnexion(connexion);
+        }
+    }
+
+    public static void addMember(String nomDB, int idProjet, int idMembre, boolean estResponsable) {
+        Connection connexion = null;
+        try {
+            connexion = CreationBD.connexionBD(nomDB);
+            String sql = "INSERT INTO ProjetMembre (id_projet, id_membre, est_responsable) VALUES (?, ?, ?)";
+
+            try (PreparedStatement statement = connexion.prepareStatement(sql)) {
+                statement.setInt(1, idProjet);
+                statement.setInt(2, idMembre);
+                statement.setBoolean(3, estResponsable);
+
+                int lines = statement.executeUpdate();
+
+                if (lines > 0) {
+                    System.out.println("Membre ajouté au projet avec succès.");
+                } else {
+                    System.out.println("Erreur lors de l'ajout du membre au projet.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            CreationBD.fermerConnexion(connexion);
+        }
+    }
+
+    public static void afficherMembresDuProjet(String nomDB, int idProjet) {
+        Connection connexion = null;
+        try {
+            connexion = CreationBD.connexionBD(nomDB);
+            String sql = "SELECT Membre.id_membre, Membre.nom, Membre.prenom, ProjetMembre.est_responsable " +
+                    "FROM Membre " +
+                    "JOIN ProjetMembre ON Membre.id_membre = ProjetMembre.id_membre " +
+                    "WHERE ProjetMembre.id_projet = ?";
+
+            try (PreparedStatement statement = connexion.prepareStatement(sql)) {
+                statement.setInt(1, idProjet);
+                ResultSet resultSet = statement.executeQuery();
+
+                System.out.println("Membres associés au projet avec ID " + idProjet + " :");
+                while (resultSet.next()) {
+                    int idMembre = resultSet.getInt("id_membre");
+                    String nom = resultSet.getString("nom");
+                    String prenom = resultSet.getString("prenom");
+                    boolean estResponsable = resultSet.getBoolean("est_responsable");
+
+                    System.out.println("ID Membre: " + idMembre + ", Nom: " + nom + ", Prénom: " + prenom + ", Responsable: " + estResponsable);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            CreationBD.fermerConnexion(connexion);
+        }
+    }
+
+    public static void passerMembreResponsable(String nomDB, int idProjet, int idMembre) {
+        Connection connexion = null;
+        try {
+            connexion = CreationBD.connexionBD(nomDB);
+            String sql = "UPDATE ProjetMembre SET est_responsable = true " +
+                    "WHERE id_projet = ? AND id_membre = ?";
+
+            try (PreparedStatement statement = connexion.prepareStatement(sql)) {
+                statement.setInt(1, idProjet);
+                statement.setInt(2, idMembre);
+
+                int rowsAffected = statement.executeUpdate();
+                if (rowsAffected > 0) {
+                    System.out.println("Le membre avec l'ID " + idMembre + " est désormais responsable du projet avec l'ID " + idProjet);
+                } else {
+                    System.out.println("Aucune modification effectuée. Vérifiez l'existence du projet et du membre.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            CreationBD.fermerConnexion(connexion);
+        }
+    }
+
 	public static int getIdMembreConnecte(String nomDB, String name, String password) throws SQLException{
 		Connection connexion = null;
 		try {
@@ -12,7 +117,7 @@ public class Requetes {
             // Reste a crypter le mot de passe
             var query = connexion.prepareStatement("SELECT id_membre FROM Auth WHERE Auth.nom_utilisateur = "+ name + " AND Auth.mdp = "+ password);
             ResultSet res = query.executeQuery();
-            int id = null;
+            int id = 0;
             try {
                 id = res.getInt("id_membre");
             }
@@ -26,14 +131,15 @@ public class Requetes {
         } finally {
             CreationBD.fermerConnexion(connexion);
         }
+        return -1;
 	}
 
 	public static boolean updateAvancementObjectif(String nomBD, String avance, int id_obj) throws SQLException{
 		Connection connexion = null;
         try {
-            connexion = CreationBD.connexionBD(nomDB);
-        	if (avance == 'En cours' || avance == 'Terminé' || avance == 'A faire') {
-        		var query = connexion.prepareStatement("UPDATE AvancementObjectif SET AvancementObjectif.avancement = "avance + " WHERE AvancementObjectif.id_objectif = "+id_obj);
+            connexion = CreationBD.connexionBD(nomBD);
+        	if (avance == "En cours" || avance == "Terminé" || avance == "A faire") {
+        		var query = connexion.prepareStatement("UPDATE AvancementObjectif SET AvancementObjectif.avancement = " +avance + " WHERE AvancementObjectif.id_objectif = "+id_obj);
         		query.executeUpdate();
         		return true;
         	}
@@ -49,7 +155,7 @@ public class Requetes {
 	public static void etatActuelProjet(String nomBD, int idProjet) {
 		Connection connexion = null;
         try {
-            connexion = CreationBD.connexionBD(nomDB);
+            connexion = CreationBD.connexionBD(nomBD);
             var query = connexion.prepareStatement("SELECT DISTINCT id_objectif, est_realise FROM Objectif WHERE id_projet = " + idProjet);
             ResultSet res = query.executeQuery();
             while(res.next()){
